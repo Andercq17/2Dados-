6ad50e2f1c97e367b181f1bf87b8fe9af9b4b114
Andersson Cardenas <john.cardenasq@upb.edu.co>
2020-08-28 22:53:05 -0500

    Versión definitiva 2Dados

---
+++ src/pkg2dados/Main.java
@@ -0,0 +1,118 @@
+
+package pkg2dados;
+       
+public class Main {
+
+    public static void main(String[] args) {
+        
+        int dado1=(int)(Math.random() *6);
+        int dado2=(int)(Math.random() *6);
+
+    switch(dado1){
+            case 1:
+                System.out.println("dado 1");
+                System.out.println("-----\n"+
+                "|o o|\n" +
+                "|o o|\n" +
+                "|o o|\n" +
+                "-----");
+                break;
+            case 2:
+                System.out.println("dado 1");
+                System.out.println("-----\n" +
+                "|o o|\n" +
+                "| o |\n" +
+                "|o o|\n" +
+                "-----");
+                break;
+            case 3:
+                System.out.println("dado 1");
+                System.out.println("-----\n" +
+                "|o o|\n" +
+                "|   |\n" +
+                "|o o|\n" +
+                "-----");
+                break;
+            case 4:
+                System.out.println("dado 1");
+                System.out.println("-----\n" +
+                "|o  |\n" +
+                "| o |\n" +
+                "|  o|\n" +
+                "-----");
+                break;
+            case 5:
+                System.out.println("dado 1");
+                System.out.println("-----\n"+
+               "|o  |\n"+
+               "|   |\n"+
+               "|  o|\n"+
+               "-----");
+                break;
+            case 6:
+                System.out.println("dado 1");
+                System.out.println("-----\n" +
+                "|   |\n" +
+                "| o |\n" +
+                "|   |\n" +
+                "-----");
+            break;
+        }
+      
+    switch(dado2){
+            case 1:
+                System.out.println("dado 2");
+                System.out.println("-----\n"+
+                "|o o|\n" +
+                "|o o|\n" +
+                "|o o|\n" +
+                "-----");
+                break;
+            case 2:
+                System.out.println("dado 2");
+                System.out.println("-----\n" +
+                "|o o|\n" +
+                "| o |\n" +
+                "|o o|\n" +
+                "-----");
+                break;
+            case 3:
+                System.out.println("dado 2");
+                System.out.println("-----\n" +
+                "|o o|\n" +
+                "|   |\n" +
+                "|o o|\n" +
+                "-----");
+                break;
+            case 4:
+                System.out.println("dado 2");
+                System.out.println("-----\n" +
+                "|o  |\n" +
+                "| o |\n" +
+                "|  o|\n" +
+                "-----");
+                break;
+            case 5:
+                System.out.println("dado 2");
+                System.out.println("-----\n"+
+               "|o  |\n"+
+               "|   |\n"+
+               "|  o|\n"+
+               "-----");
+                break;
+            case 6:
+                System.out.println("dado 2");
+                System.out.println("-----\n" +
+                "|   |\n" +
+                "| o |\n" +
+                "|   |\n" +
+                "-----");
+            
+            break; 
+        }
+    }
+}
+    
+    
+    
+
